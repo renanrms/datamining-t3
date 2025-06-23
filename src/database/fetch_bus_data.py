0c71112dc2eb236709dbd089751d5a9070384640
Renan Passos <renanvmsp@gmail.com>
2025-06-22 23:45:28 -0300

    Encontra ponto final.

--- src/database/fetch_bus_data.py
+++ src/database/fetch_bus_data.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 
 
-def fetch_bus_data(connection, linha=None, limit=None):
+def fetch_bus_data(connection, linha=None, hour_range: tuple[int, int] = (8, 20), require_no_stopped=False, limit=None):
     query = f"""
     SELECT
         ordem,
@@ -11,7 +11,8 @@ def fetch_bus_data(connection, linha=None, limit=None):
         datahoraservidor
     FROM tracking
         {f"WHERE linha = '{linha}' AND" if linha else ''}
-        EXTRACT(HOUR FROM datahoraservidor) BETWEEN 8 AND 22
+        {f"EXTRACT(HOUR FROM datahoraservidor) BETWEEN {hour_range[0]} AND {hour_range[1]}"}
+        {"AND velocidade > 0" if require_no_stopped else ''}
     ORDER BY datahoraservidor
     {f'LIMIT {limit}' if limit else ''}
     """
